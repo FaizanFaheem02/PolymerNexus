1014eba93c8e6634e43e8229dd6cc85e8e8bf1c9
FaizanFaheem02 <faixan.faheem@gmail.com>
2026-07-14 23:27:50 +0200

    Minor improvement

--- generate_r2rml.py
+++ generate_r2rml.py
@@ -4,7 +4,7 @@ import pandas as pd
 from pathlib import Path
 from dotenv import load_dotenv
 
-# Load environment variables from the project-root .env file
+# Load environment variables from the .env file
 BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(BASE_DIR / ".env")
 
